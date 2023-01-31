d829358b284198ca385cdf35aa86d126179afc2d
hw commit <demaniks@yahoo.com>
2023-01-31 17:50:51 +0300

    Начали

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{45, 56, 585, 452, 785, 45, 78, 96};
+
+
+
+    }
+
+
+
+}
