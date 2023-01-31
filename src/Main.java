15567fc34083366868bb6beb343e6c8ab327bd97
hw commit <demaniks@yahoo.com>
2023-01-31 18:10:23 +0300

    ДЗ 1-1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] arr = new Integer[]{45, 56, 585, 452, 785, 45, 78, 96};
-
+        Stream<Integer> arr = new ArrayList<>(Arrays.asList(45, 56, 585, 452, 785, 45, 78, 96)).stream();
+        findMinMax(
+                arr,
+                Comparator.naturalOrder(),
+                (e, s) -> System.out.println(String.format("min: " + e + " max: " + s))
+        );
 
 
     }
 
-
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> arrayList = new ArrayList<>();
+        T min = null;
+        T max = null;
+        arrayList = stream
+                .sorted(order)
+                .collect(Collectors.toList());
+        if (arrayList.size() != 0) {
+            min = arrayList.get(0);
+            max = arrayList.get(arrayList.size() - 1);
+            minMaxConsumer.accept(min, max);
+            if (min == null && max == null) {
+                minMaxConsumer.accept(null, null);
+            }
+        }
+    }
 
 }
